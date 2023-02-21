bd6efe16ed3bee6b631f910467eafc3dad57548d
ersinelci <ersinelci@gmail.com>
2023-02-21 20:01:07 +0300

    last changes

--- Day12-saat4/my-app-v2/src/app/shared/shared.module.ts
+++ Day12-saat4/my-app-v2/src/app/shared/shared.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AppRoutingModule } from '../app-routing.module';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { AdminLayoutComponent } from './admin-layout/admin-layout.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MainLayoutComponent,
+    AdminLayoutComponent
   ],
   exports:[
     HeaderComponent,
