663fad2e1e7434c1d1f21a4cc332a0b174918a31
ersinelci <ersinelci@gmail.com>
2023-02-21 11:57:40 +0300

    Day 12 - öğleden önce

---
+++ Day 12/my-app/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InlineComponent } from './components/inline-component/inline.component';
+
+import { NormalComponent } from './components/normal/normal.component';
+import { StructuralDirectiveComponent } from './components/structural-directive/structural-directive.component';
+import { ExampleComponent } from './components/example/example.component';
+import { CustomDirective } from './directives/custom.directive';
+import { ExamplePipeComponent } from './pipes/example-pipe/example-pipe.component';
+import { CustomUppercasePipe } from './pipes/custom-uppercase.pipe';
+import { CustomerModule } from './customer/customer.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InlineComponent,
+    NormalComponent,
+    StructuralDirectiveComponent,
+    ExampleComponent,
+    CustomDirective,
+    ExamplePipeComponent,
+    CustomUppercasePipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CustomerModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
