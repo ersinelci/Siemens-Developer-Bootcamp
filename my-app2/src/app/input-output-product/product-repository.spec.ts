93d32a0bc4589fa5a0ebdcb06fed3d7688a1b085
Ersin Elci <ersinelci@gmail.com>
2023-02-23 08:59:07 +0300

    Delete my-app2 directory

--- my-app2/src/app/input-output-product/product-repository.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ProductRepository } from './product-repository';
-
-describe('ProductRepository', () => {
-  it('should create an instance', () => {
-    expect(new ProductRepository()).toBeTruthy();
-  });
-});
