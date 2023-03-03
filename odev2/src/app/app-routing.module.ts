4bd512905512d4eafb12b9a593cfc16dc7fd8827
ersinelci <ersinelci@gmail.com>
2023-03-03 10:45:48 +0300

    program architecture was created

--- odev2/src/app/app-routing.module.ts
+++ odev2/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VisitorComponent } from './admin/visitor/visitor.component';
+import { AboutComponent } from './main/about/about.component';
+import { ContactComponent } from './main/contact/contact.component';
+import { HomeComponent } from './main/home/home.component';
+import { AdminLayoutComponent } from './shared/admin-layout/admin-layout.component';
+import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:MainLayoutComponent, children: [
+    {path: '',component:HomeComponent},
+    {path: 'anasayfa', component:HomeComponent},
+    {path: 'hakkimizda', component:AboutComponent},
+    {path: 'iletisim', component:ContactComponent}
+  ]},
+  {path: 'admin', component:AdminLayoutComponent, children: [
+    {path: 'ziyaretci', component: VisitorComponent}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- odev2/src/app/app.module.ts
+++ odev2/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AdminModule } from './admin/admin.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './main/about/about.component';
+import { ContactComponent } from './main/contact/contact.component';
+import { HomeComponent } from './main/home/home.component';
+import { SharedModule } from './shared/shared.module';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AdminModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
