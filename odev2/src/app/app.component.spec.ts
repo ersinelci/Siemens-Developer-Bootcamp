810a65856e42e375e8d24c32ef3d564c29f7687d
ersinelci <ersinelci@gmail.com>
2023-03-03 10:31:24 +0300

    odev2 first commit

--- odev2/src/app/app.component.spec.ts
+++ odev2/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'myapp-inclass'`, () => {
+  it(`should have as title 'odev2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('myapp-inclass');
+    expect(app.title).toEqual('odev2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('myapp-inclass app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('odev2 app is running!');
   });
 });
